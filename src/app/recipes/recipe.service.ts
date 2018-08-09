5c39e831a31c260836d9c7ac0b663b68e68a1b41
nakuyabridget <bnakuya7@gmail.com>
2018-08-09 12:52:42 +0300

    inserting ingredients to our recipes using the recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,18 +8,24 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
+  // inserting ingedients to our recipe using the recipe service
   private recipes: Recipe[] = [
     new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+
+      'Plantain',
+      'The best in town for sure',
+      'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg',
+
       [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
+        new Ingredient('Onions', 2),
+        new Ingredient('French Fries', 20),
+        new Ingredient('dodo', 2)
       ]),
+
     new Recipe('Big Fat Burger',
       'What else you need to say?',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
